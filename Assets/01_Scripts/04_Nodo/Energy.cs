449d025fa6bb3a3b0c69160c25803a1e5ee97ced
AlejandroV-alpha <va0035314@est.univalle.edu>
2025-11-08 04:07:21 -0400

    108-A add: energy scene, energy that enemies will release for the player

---
+++ Assets/01_Scripts/04_Nodo/Energy.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+/// <summary>
+/// Componente que transfiere una cantidad definida de energia a otro objeto
+/// que implemente la interfaz ITakeEnergy al detectar una colision tipo Trigger.
+/// Tambien se autodestruye despues de un tiempo definido.
+/// </summary>
+public class Energy : MonoBehaviour
+{
+    [SerializeField] float energy = 5f;
+    [SerializeField] float timeToDestroy = 10f;
+
+    void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    /// <summary>
+    /// Se ejecuta automaticamente cuando otro Collider2D entra en contacto con este objeto.
+    /// Si el objeto colisionado implementa ITakeEnergy, se le transfiere la energia definida.
+    /// Luego, este objeto se destruye inmediatamente.
+    /// </summary>
+    /// <param name="collision">El collider del objeto que entra en contacto.</param>
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        ITakeEnergy takeEnergy = collision.GetComponent<ITakeEnergy>();
+        if (takeEnergy != null && collision.gameObject.CompareTag("Player"))
+        {
+            takeEnergy.TakeEnergy(energy);
+            Destroy(gameObject);
+        }
+    }
+}
